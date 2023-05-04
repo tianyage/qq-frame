08bedb06c7a4aa14f92d8b6898761c817ac14492
tianyage <454701103@qq.com>
2023-05-04 15:10:26 +0800

    changed

--- src/QF.php
+++ src/QF.php
@@ -4,12 +4,11 @@ namespace Tianyage\QqFrame;
 
 use Tianyage\QqFrame\lib\Qy as qy;
 use Tianyage\QqFrame\lib\Xlz as xlz;
-use Tianyage\QqFrame\lib\Wq as wq;
 
 class QF
 {
     
-    public qy|xlz|wq $sdk;
+    public qy|xlz $sdk;
     
     /**
      *
@@ -17,9 +16,9 @@ class QF
      * @param int    $robot 框架QQ
      * @param int    $port  api端口
      *
-     * @return qy|xlz|wq
+     * @return qy|xlz
      */
-    final public function init(string $host, int $robot, int $port): qy|xlz|wq
+    final public function init(string $host, int $robot, int $port): qy|xlz
     {
         if ($port === 3000 || $port === 3001) {
             $this->sdk = new xlz();
@@ -32,11 +31,26 @@ class QF
         return $this->sdk;
     }
     
+    /**
+     * 如果调用的方法不能访问，它将被触发
+     *
+     * @param $name
+     * @param $arguments
+     *
+     * @return mixed
+     */
     public function __call($name, $arguments)
     {
         return $this->sdk->$name() ?? false;
     }
     
+    /**
+     * 调用的属性不能访问(比如private)，它将被触发
+     *
+     * @param $name
+     *
+     * @return false
+     */
     public function __get($name)
     {
         return $this->$name ?? false;
