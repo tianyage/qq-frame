3a66a46eb9a876049eed5363fe5bc1e57ef8e4b8
tianyage <454701103@qq.com>
2024-04-21 02:42:00 +0800

    update

--- src/lib/Qy.php
+++ src/lib/Qy.php
@@ -388,12 +388,12 @@ class Qy extends Common
      * 名片赞
      * (非好友情况下进行点赞时返回成功，但不一定真正点上了，对方开启陌生人点赞时才能点上(手Q默认关闭陌生人点赞))
      *
-     * @param string $toqq 对方QQ
-     * @param int    $num  点赞次数 默认1
+     * @param string|int $toqq 对方QQ
+     * @param int        $num  点赞次数 默认1
      *
      * @return string
      */
-    public function cardLike(string $toqq, int $num = 1): string
+    public function cardLike(string|int $toqq, int $num = 1): string
     {
         // {"server_info":{"key":"123","port":"4001","serverUrl":"http://192.168.11.1"},"type":"Event","data":{"框架QQ":"908777454","操作QQ":"0","触发QQ":"454701103","来源群号":"0","来源群名":"","消息内容":"赞了我的资料卡1次","消息类型":"108","操作QQ昵称":"","触发QQ昵称":"simon\\u2776","消息子类型":"10021","消息Seq":"0","消息时间戳":"1679587653"}}
         $num = max($num, 1); // 最少点赞一次
@@ -474,13 +474,13 @@ class Qy extends Common
      * 名片赞2
      * （发功能包版，一次性点赞，可以减少接口访问次数了）
      *
-     * @param string $toqq 对方QQ
-     * @param int    $num  点赞次数 默认1
-     * @param int    $type 点赞类型 1好友 27随心贴陌生人（好像无效）  31搜索QQ（好像无效）  5群友  12我赞过谁  41附近的人
+     * @param string|int $toqq 对方QQ
+     * @param int        $num  点赞次数 默认1
+     * @param int        $type 点赞类型 1好友 27随心贴陌生人（好像无效）  31搜索QQ（好像无效）  5群友  12我赞过谁  41附近的人
      *
      * @return string
      */
-    public function cardLike2(string $toqq, int $num = 1, int $type = 1): string
+    public function cardLike2(string|int $toqq, int $num = 1, int $type = 1): string
     {
         // {"server_info":{"key":"123","port":"4001","serverUrl":"http://192.168.11.1"},"type":"Event","data":{"框架QQ":"908777454","操作QQ":"0","触发QQ":"454701103","来源群号":"0","来源群名":"","消息内容":"赞了我的资料卡1次","消息类型":"108","操作QQ昵称":"","触发QQ昵称":"simon\\u2776","消息子类型":"10021","消息Seq":"0","消息时间戳":"1679587653"}}
         
